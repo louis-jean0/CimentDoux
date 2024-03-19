27842cdee315f7904483b6c3255f1e5bb3106b0f
louis <louis.jean34@hotmail.fr>
2024-03-19 19:55:52 +0100

    Setup codebase for TP3

---
+++ moteur/src/main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <Window.hpp>
+
+int main(int argc, char* argv[]) {
+    Window window();
+    
+}
